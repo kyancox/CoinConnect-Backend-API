cc5c6251350e159e198cc3fab850dd3a31fd1cbb
Kyan Cox <kyan.cox@gmail.com>
2024-01-18 22:50:45 -0500

    Modifications made to POST routes for frontend.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, jsonify, send_file, render_template, url_for, redirect, Response, abort, jsonify
+from flask import Flask, request, jsonify, send_file, render_template, url_for, redirect, Response, abort, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
+import uuid
 from requests.exceptions import JSONDecodeError as RequestsJSONDecodeError
 from werkzeug.utils import secure_filename
 from io import BytesIO
@@ -17,6 +20,8 @@ from ledger import ledgerPortfolio
 from portfolioClass import Portfolio, MasterPortfolio
 
 app = Flask(__name__)
+CORS(app)
+app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
 db = SQLAlchemy(app)
 
@@ -32,24 +37,29 @@ class Upload(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(50))
     data = db.Column(db.LargeBinary)
+    session_id = db.Column(db.String(200), nullable=False)  # Link to the session
 
 class ApiKey(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     service = db.Column(db.String(50), nullable=False)  # 'Coinbase', 'Gemini', etc.
     api_key = db.Column(db.String(200), nullable=False)
     api_secret = db.Column(db.String(200), nullable=False)
+    session_id = db.Column(db.String(200), nullable=False)  # Link to the session
 
 class PortfolioDB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     account = db.Column(db.String(50), nullable=False)
     portfolio_data  = db.Column(db.Text, nullable=False)
     total_balance = db.Column(db.String(100), nullable=False)
+    session_id = db.Column(db.String(200), nullable=False)  # Link to the session
 
 class MasterDB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     portfolio_data  = db.Column(db.Text, nullable=False)
     total_balance = db.Column(db.String(100), nullable=False)
     excel_bytes = db.Column(db.LargeBinary)
+    session_id = db.Column(db.String(200), nullable=False)  # Link to the session
+
 
 # Post Coinbase Keys
 @app.route('/api/coinbase/keys', methods=['POST'])
@@ -59,34 +69,40 @@ def set_coinbase_keys():
 
     # Check if data is None or if 'api_key' and 'api_secret' are present in the data
     if data is None or 'api_key' not in data or 'api_secret' not in data:
-        return jsonify({'error': 'Invalid request format. Please provide "api_key" and "api_secret" in the request body.'}), 400
+        return jsonify({'message': 'Invalid request format. Please provide "api_key" and "api_secret" in the request body.'}), 400
 
     key = data['api_key']
     secret = data['api_secret']
 
-    coinbase_keys = ApiKey.query.filter_by(service='Coinbase').first()
+
+    if 'session_id' not in session:
+        session['session_id'] = str(uuid.uuid4())
+
+    session_id = session['session_id']
+
+    coinbase_keys = ApiKey.query.filter_by(service='Coinbase', session_id=session['session_id']).first()
 
     if coinbase_keys:
         coinbase_keys.api_key = key
         coinbase_keys.api_secret = secret
     else:
-        new_coinbase_keys = ApiKey(service='Coinbase', api_key=key, api_secret=secret)
+        new_coinbase_keys = ApiKey(service='Coinbase', api_key=key, api_secret=secret, session_id = session_id )
         db.session.add(new_coinbase_keys)
     db.session.commit()
 
-    init_coinbase()
-    return jsonify({'message':'Coinbase keys updated successfully'}), 201
+    return init_coinbase()
 
 def init_coinbase():
 
-    coinbase_keys = ApiKey.query.filter_by(service='Coinbase').first()
+    coinbase_keys = ApiKey.query.filter_by(service='Coinbase', session_id=session['session_id']).first()
 
     if coinbase_keys and coinbase_keys.api_key and coinbase_keys.api_secret:
 
         key = coinbase_keys.api_key
         secret = coinbase_keys.api_secret
+        session_id = session['session_id']
 
-        coinbaseModel = PortfolioDB.query.filter_by(account='Coinbase').first()
+        coinbaseModel = PortfolioDB.query.filter_by(account='Coinbase', session_id=session_id).first()
 
         try:
             coinbase = coinbasePortfolio(key, secret)
@@ -99,19 +115,21 @@ def init_coinbase():
             else:
                 coinbaseModel = PortfolioDB(account=coinbase.accountName,
                                             portfolio_data = portfolio_json, 
-                                            total_balance=coinbase.totalBalance()) 
+                                            total_balance=coinbase.totalBalance(),
+                                            session_id=session_id) 
                 db.session.add(coinbaseModel)
             db.session.commit()
 
             print('Coinbase portfolio initialized successfully')
+            return jsonify({'message':'Coinbase keys updated successfully'}), 201
         except RequestsJSONDecodeError:
             #return jsonify({'message':'api_key or api_secret for Coinbase was invalid.'}), 404
-            abort(404, 'api_key or api_secret for Coinbase was invalid.')
+            return jsonify({'message':'api_key or api_secret for Coinbase was invalid.'}), 404
         
         # accounts.append(coinbase)
         # print("coinbase account appended to accounts")
     else:
-        abort(404, 'api_key or api_secret for Coinbase was not uploaded.')
+        return jsonify({'message':'api_key or api_secret for Coinbase was not uploaded.'}), 404
 
 
 # Post Gemini Keys
@@ -121,34 +139,39 @@ def set_gemini_keys():
 
     # Check if data is None or if 'api_key' and 'api_secret' are present in the data
     if data is None or 'api_key' not in data or 'api_secret' not in data:
-        return jsonify({'error': 'Invalid request format. Please provide "api_key" and "api_secret" in the request body.'}), 400
+        return jsonify({'message': 'Invalid request format. Please provide "api_key" and "api_secret" in the request body.'}), 400
     
     key = data['api_key']
     secret = data['api_secret']
 
-    gemini_keys = ApiKey.query.filter_by(service='Gemini').first()
+    if 'session_id' not in session:
+        session['session_id'] = str(uuid.uuid4())
+
+    session_id = session['session_id']
+
+    gemini_keys = ApiKey.query.filter_by(service='Gemini', session_id=session_id).first()
 
     if gemini_keys:
         gemini_keys.api_key = key
         gemini_keys.api_secret = secret
     else:
-        new_gemini_keys = ApiKey(service='Gemini',api_key=key, api_secret=secret)
+        new_gemini_keys = ApiKey(service='Gemini',api_key=key, api_secret=secret, session_id=session_id)
         db.session.add(new_gemini_keys)
     db.session.commit()
 
-    init_gemini()
-    return jsonify({'message':'Gemini keys updated successfully'}), 201
+    return init_gemini()
 
 def init_gemini():
+    session_id=session['session_id']
 
-    gemini_keys = ApiKey.query.filter_by(service='Gemini').first()
+    gemini_keys = ApiKey.query.filter_by(service='Gemini',session_id=session_id).first()
 
     if gemini_keys and gemini_keys.api_key and gemini_keys.api_secret:
 
         key = gemini_keys.api_key
         secret = gemini_keys.api_secret
 
-        geminiModel = PortfolioDB.query.filter_by(account='Gemini').first()
+        geminiModel = PortfolioDB.query.filter_by(account='Gemini', session_id=session_id).first()
 
         try:
             gemini = geminiPortfolio(key, secret)
@@ -161,19 +184,21 @@ def init_gemini():
             else:
                 geminiModel = PortfolioDB(account=gemini.accountName,
                                           portfolio_data=portfolio_json,
-                                          total_balance=gemini.totalBalance())
+                                          total_balance=gemini.totalBalance(),
+                                          session_id=session_id)
                 db.session.add(geminiModel)
             db.session.commit()
 
             print('Gemini portfolio initialized successfully')
+            return jsonify({'message':'Gemini keys updated successfully'}), 201
         except Exception:
             #return jsonify({'message':'api_key or api_secret for Gemini was invalid.'}), 404
-            abort(404, 'api_key or api_secret for Gemini was invalid.')
+            return jsonify({'message':'api_key or api_secret for Gemini was invalid.'}), 404
         
         # accounts.append(gemini)
         # print("gemini account appended to accounts")
     else:
-        abort(404, 'api_key or api_secret for Gemini was not uploaded.')
+        return jsonify({'message':'api_key or api_secret for Coinbase was not uploaded.'}), 404
 
 # Post Ledger CSV Data
 @app.route('/api/ledger/upload-csv', methods=['POST', 'GET'])
@@ -181,40 +206,47 @@ def upload_ledger_csv():
     if request.method == 'POST':
         # Check if the POST request has a file part
         if 'file' not in request.files:
-            return {'error': 'No file part'}, 400
+            return jsonify({'message': 'No file part'}), 400
 
         file = request.files['file']
 
         # Check if the file is empty
         if file.filename == '':
-            return {'error': 'No selected file'}, 400
+            return jsonify({'message': 'No selected file'}), 400
         
         # Check if the file is an Excel file
         if not file.filename.endswith('.csv'):
-            return {'error': 'Invalid file format. Please upload a CSV file (.csv)'}, 400
+            print('Invalid file type error reached.')
+            return jsonify({'message': 'Invalid file format. Please upload a CSV file (.csv)'}), 400
         
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
 
-            existing_upload = Upload.query.first()
+            if 'session_id' not in session:
+                session['session_id'] = str(uuid.uuid4())
+
+            session_id = session['session_id']
+
+            existing_upload = Upload.query.filter_by(session_id=session_id).first()
 
             if existing_upload:
                 existing_upload.filename = filename
                 existing_upload.data = file.read()
             else:
-                upload = Upload(filename = filename, data = file.read())
+                upload = Upload(filename = filename,
+                                data = file.read(),
+                                session_id=session_id)
                 db.session.add(upload)
             db.session.commit()
 
             # upload = Upload.query.order_by(Upload.id.desc()).first()
             # if upload is None:
-            #     return jsonify({'error': 'No ledger file found'}), 404
+            #     return jsonify({'message': 'No ledger file found'}), 404
             # file_data = BytesIO(upload.data)
 
-            init_ledger()
-            return redirect(url_for('download_page'))
+            return init_ledger()
         
-        return 'Upload failed.'
+        return jsonify({'message': 'Upload failed.'}), 404
     
     return render_template('upload.html')
 
@@ -223,14 +255,16 @@ def download_page():
     return render_template('download.html')
 
 def init_ledger():
-    upload = Upload.query.order_by(Upload.id.desc()).first()
+    session_id = session['session_id']
+    #upload = Upload.query.order_by(Upload.id.desc()).first()
+    upload = Upload.query.filter_by(session_id=session_id).first()
 
     if upload is None:
-        return jsonify({'error': 'No ledger file found'}), 404
+        return jsonify({'message': 'No ledger file found'}), 404
     
     file_data = BytesIO(upload.data)
 
-    ledgerModel = PortfolioDB.query.filter_by(account='Ledger').first()
+    ledgerModel = PortfolioDB.query.filter_by(account='Ledger', session_id=session_id).first()
 
     try:
         ledger = ledgerPortfolio(file_data)
@@ -243,21 +277,24 @@ def init_ledger():
         else:
             ledgerModel = PortfolioDB(account=ledger.accountName,
                                       portfolio_data=portfolio_json,
-                                      total_balance=ledger.totalBalance())
+                                      total_balance=ledger.totalBalance(),
+                                      session_id=session_id)
             db.session.add(ledgerModel)
         db.session.commit()
 
         print('Ledger portfolio initialized successfully')
+        return jsonify({'message': 'Ledger CSV updated successfully.'}), 201
     except Exception:
-        abort(404, 'CSV file for Ledger was invalid.')
+        return jsonify({'message': 'CSV file for Ledger was invlaid.'}), 404
     
     # accounts.append(ledger)
     # print("ledger account appended to accounts")
 
 def init_master():
-    coinbaseModel = PortfolioDB.query.filter_by(account='Coinbase').first()
-    geminiModel = PortfolioDB.query.filter_by(account='Gemini').first()
-    ledgerModel = PortfolioDB.query.filter_by(account='Ledger').first()
+    session_id = session['session_id']
+    coinbaseModel = PortfolioDB.query.filter_by(account='Coinbase', session_id=session_id).first()
+    geminiModel = PortfolioDB.query.filter_by(account='Gemini', session_id=session_id).first()
+    ledgerModel = PortfolioDB.query.filter_by(account='Ledger', session_id=session_id).first()
     accounts = []
     if coinbaseModel:
         portfolio_dict = json.loads(coinbaseModel.portfolio_data)
@@ -292,8 +329,9 @@ def init_master():
         masterModel = MasterDB(
             portfolio_data=portfolio_json,
             total_balance=master.totalBalance(),
-            excel_bytes=excel_bytes_io.read()
-            )
+            excel_bytes=excel_bytes_io.read(),
+            session_id=session_id
+        )
         db.session.add(masterModel)
     db.session.commit()
     
@@ -301,7 +339,7 @@ def init_master():
 # Return Coinbase Loaded JSON
 @app.route('/api/coinbase/json', methods=['GET'])
 def coinbase_json():    
-    coinbase = PortfolioDB.query.filter_by(account='Coinbase').first()
+    coinbase = PortfolioDB.query.filter_by(account='Coinbase', session_id=session['session_id']).first()
 
     if coinbase:
         data = coinbase.portfolio_data
@@ -313,7 +351,7 @@ def coinbase_json():
 # Return Coinbase Total Balance
 @app.route('/api/coinbase/total-balance', methods=['GET'])
 def coinbase_total_balance():
-    coinbase = PortfolioDB.query.filter_by(account='Coinbase').first()
+    coinbase = PortfolioDB.query.filter_by(account='Coinbase', session_id=session['session_id']).first()
 
     if coinbase:
         return jsonify({'balance':coinbase.total_balance}), 202
@@ -323,7 +361,7 @@ def coinbase_total_balance():
 # Return Gemini Loaded JSON
 @app.route('/api/gemini/json', methods=['GET'])
 def gemini_json():
-    gemini = PortfolioDB.query.filter_by(account='Gemini').first()
+    gemini = PortfolioDB.query.filter_by(account='Gemini', session_id=session['session_id']).first()
 
     if gemini:
         data = gemini.portfolio_data
@@ -335,7 +373,7 @@ def gemini_json():
 # Return Gemini Total Balance
 @app.route('/api/gemini/total-balance', methods=['GET'])
 def gemini_total_balance():
-    gemini = PortfolioDB.query.filter_by(account='Gemini').first()
+    gemini = PortfolioDB.query.filter_by(account='Gemini', session_id=session['session_id']).first()
 
     if gemini:
         return jsonify({'balance':gemini.total_balance}), 202
@@ -345,7 +383,7 @@ def gemini_total_balance():
 # Return Ledger Loaded JSON
 @app.route('/api/ledger/json', methods=['GET'])
 def ledger_json():
-    ledger = PortfolioDB.query.filter_by(account='Ledger').first()
+    ledger = PortfolioDB.query.filter_by(account='Ledger', session_id=session['session_id']).first()
 
     if ledger:
         data = ledger.portfolio_data
@@ -357,7 +395,7 @@ def ledger_json():
 # Return Ledger Total Balance
 @app.route('/api/ledger/total-balance', methods=['GET'])
 def ledger_total_balance():
-    ledger = PortfolioDB.query.filter_by(account='Ledger').first()
+    ledger = PortfolioDB.query.filter_by(account='Ledger', session_id=session['session_id']).first()
 
     if ledger:
         return jsonify({'balance':ledger.total_balance}), 202
@@ -369,7 +407,7 @@ def ledger_total_balance():
 def master_json():
     init_master()
 
-    master = MasterDB.query.first()
+    master = MasterDB.query.filter_by(session_id=session['session_id']).first()
 
     if master:
         data = master.portfolio_data
@@ -383,7 +421,7 @@ def master_json():
 def master_total_balance():
     init_master()
 
-    master = MasterDB.query.first()
+    master = MasterDB.query.filter_by(session_id=session['session_id']).first()
 
     if master:
         return jsonify({'balance':master.total_balance}), 202
@@ -394,7 +432,7 @@ def master_total_balance():
 @app.route('/api/master/download-xlsx', methods = ['GET'])
 def download_master_xlsx():
     init_master()
-    master = MasterDB.query.first()
+    master = MasterDB.query.filter_by(session_id=session['session_id']).first()
 
     excel_file = master.excel_bytes
 
@@ -410,7 +448,7 @@ def download_master_xlsx():
                         download_name=fileName,
                         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     
-    return jsonify({'error':'No CSV file found'}), 404
+    return jsonify({'message':'No CSV file found'}), 404
 
 if __name__ == "__main__":
     with app.app_context():
